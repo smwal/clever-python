183ace8c147e2d664ae69d2982f5c46fd8846bfd
ameliez <amelie.zeng@clever.com>
2016-06-04 23:54:24 -0700

    Update app.py

--- bin/app.py
+++ bin/app.py
@@ -10,7 +10,7 @@ urls = (
 
 client_id=os.environ['CLEVER_CLIENT_ID']
 client_secret=os.environ['CLEVER_CLIENT_SECRET']
-redirect_uri="https://squidword.herokuapp.com/oauth/clever"
+redirect_uri="https://squidword.herokuapp.com/oauth/clever/"
 
 app = web.application(urls, globals())
 
