404a684c7b5148d6174a974d4972bf9f689194b4
ameliez <amelie.zeng@clever.com>
2016-06-04 23:23:27 -0700

    First Commit
    Uploading files

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description' : 'Integrating with Clever Instant Login',
+	'author' : 'Amelie Zeng',
+	'url' : 'URL to get it at',
+	'download_url' : 'Where to download it',
+	'author_email' : 'amezen1@gmail.com',
+	'version' : '0.1',
+	'install_requires' : ['nose'],
+	'packages' : ['SquidWord'],
+	'scripts' : [],
+	'name' : 'SquidWord'
+}
+
+setup (**config)
